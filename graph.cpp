00e1409a025bde030625928361d5ca3e425ca989
Daniel Soncco <chen.soncco@gmail.com>
2015-03-12 19:21:50 +0100

    plot the matrix

---
+++ graph.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define f(i,x,y) for (int i = x; i < y; i++)
+#define fd(i,x,y) for(int i = x; i>= y; i--)
+#define FOR(it,A) for(typeof A.begin() it = A.begin(); it!=A.end(); it++)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define vint vector<int>
+#define ll long long
+#define clr(A,x) memset(A, x, sizeof A)
+#define pb push_back
+#define pii pair<int,int>
+#define fst first
+#define snd second
+#define ones(x) __builtin_popcount(x)
+#define cua(x) (x)*(x)
+#define eps (1e-9)
+#define oo (1<<30)
+#define debug(x) cout <<#x << " = " << x << endl
+#define adebug(x,n) cout <<#x<<endl; f(i,0,n)cout<<x[i]<<char(i+1==n?10:32)
+#define mdebug(x,m,n) cout <<#x<<endl; f(i,0,m)f(j,0,n)cout<<x[i][j]<<char(j+1==n?10:32)
+#define N 1
+using namespace std;
+template<class T> inline void mini(T &a,T b){if(b<a) a=b;}
+template<class T> inline void maxi(T &a,T b){if(b>a) a=b;}
+
+
+
+int A[16][100];
+
+int main(){
+	int r,s,u,p,m;
+	cin >> r >> s >> u >> p >> m;
+	f(t,0,u) {
+		int x,y;
+		scanf("%d%d", &x, &y);
+		A[x][y] = 1;
+	}
+	f(i,0,16) {
+		f(j,0,100) printf("%d", A[i][j]);
+		putchar(10);
+	}
+}
+
+
